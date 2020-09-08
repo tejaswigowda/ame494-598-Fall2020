ca96ffb6a8d048b0226a31ac4263bcb62ed2f660
tejaswigowda <tejaswi@foxyninjastudios.com>
2020-09-08 11:58:28 -0700

    ac

---
+++ HelloWorldWatch/helloWorld/helloWorld.ino
@@ -0,0 +1,19 @@
+#include <TTGO.h>
+TTGOClass *ttgo;
+
+void setup()
+{
+    Serial.begin(115200);
+    ttgo = TTGOClass::getWatch();
+    ttgo->begin();
+    ttgo->openBL();
+
+    ttgo->eTFT->fillScreen(TFT_BLACK);
+    ttgo->eTFT->setTextColor(TFT_WHITE, TFT_BLACK);
+    ttgo->eTFT->setTextFont(4);
+    ttgo->eTFT->drawString("Hello World",  5, 10);
+}
+
+void loop()
+{ 
+}
