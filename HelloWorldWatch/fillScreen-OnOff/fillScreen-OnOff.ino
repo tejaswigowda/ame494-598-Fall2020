8a214d057c71365880cfbf52783dd2c17c706e42
tejaswigowda <tejaswi@foxyninjastudios.com>
2020-09-15 14:44:56 -0700

    in class

--- HelloWorldWatch/fillScreen-OnOff/fillScreen-OnOff.ino
+++ HelloWorldWatch/fillScreen-OnOff/fillScreen-OnOff.ino
@@ -20,11 +20,6 @@ void setup()
     ttgo->begin();
     ttgo->openBL();
 
-    ttgo->tft->fillScreen(TFT_BLACK);
-    ttgo->tft->setTextColor(TFT_WHITE, TFT_BLACK);
-    ttgo->tft->setTextFont(4);
-    ttgo->tft->drawString("User Button released",  5, 100, 4);
-
     ttgo->button->setPressedHandler(pressed);
     ttgo->button->setReleasedHandler(released);
 
@@ -33,10 +28,10 @@ void setup()
 void loop()
 {
     ttgo->button->loop();
+      Serial.println(screenOn);
 
-
-    Serial.println("R");
     if(screenOn){
+      Serial.println("R");
       ttgo->tft->fillScreen(TFT_RED);
       delay(1000);
       Serial.println("G");
@@ -47,7 +42,7 @@ void loop()
       delay(1000);
     }
     else{
-            ttgo->tft->fillScreen(TFT_BLACK);
+       ttgo->tft->fillScreen(TFT_BLACK);
     }
     
 }
