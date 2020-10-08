fef95d0d6e80161264161e5fb0561cb77ae9f58b
tejaswigowda <tejaswi@foxyninjastudios.com>
2020-10-08 14:48:46 -0700

    in class

--- watchSensors/Accel-httpClient/Accel-httpClient.ino
+++ watchSensors/Accel-httpClient/Accel-httpClient.ino
@@ -23,7 +23,7 @@ unsigned long lastTime = 0;
 // Timer set to 10 minutes (600000)
 //unsigned long timerDelay = 600000;
 // Set timer to 5 seconds (5000)
-unsigned long timerDelay = 5000;
+unsigned long timerDelay = 300;
 
 String response;
 
