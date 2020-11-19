388b9d024a7f97bf5200d213a7ef11a8e6d47e31
tejaswigowda <tejaswi@foxyninjastudios.com>
2020-11-19 14:08:56 -0700

    in class

--- smartButton/mcu/mcu.ino
+++ smartButton/mcu/mcu.ino
@@ -3,12 +3,61 @@
 
 TTGOClass *ttgo;
 
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+const char* ssid = "NETGEAR31";
+const char* password = "fluffywind2904";
+
+//Your Domain name with URL path or IP address with path
+const char* serverName = "http://192.168.0.200:1234/sendEmail";
+
+String response;
+
+
+String httpGETRequest(const char* serverName) {
+  HTTPClient http;
+    
+  // Your IP address with path or Domain name with URL path 
+  http.begin(serverName);
+  
+  // Send HTTP POST request
+  int httpResponseCode = http.GET();
+  
+  String payload = "{}"; 
+  
+  if (httpResponseCode>0) {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    payload = http.getString();
+  }
+  else {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+  // Free resources
+  http.end();
+
+  return payload;
+}
+
 void pressed()
 {
     uint16_t color = random(0xFFFF);
     ttgo->tft->fillScreen(color);
     ttgo->tft->setTextColor(color, TFT_WHITE);
     ttgo->tft->drawString("User Button pressed",  5, 100, 4);
+
+    if(WiFi.status()== WL_CONNECTED){
+              
+      response = httpGETRequest(serverName);
+      Serial.println(response);
+
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+    
 }
 void released()
 {
@@ -32,6 +81,19 @@ void setup()
 
     ttgo->button->setPressedHandler(pressed);
     ttgo->button->setReleasedHandler(released);
+
+    
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+
+ 
 
 }
 
